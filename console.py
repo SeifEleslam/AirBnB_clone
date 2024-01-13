6bfa6734a675729a5492d0aab4d2ca167c6f14ef
SeifEleslam <seifgouda98@gmail.com>
2024-01-13 15:08:17 +0200

    test

--- console.py
+++ console.py
@@ -135,6 +135,7 @@ class HBNBCommand(Cmd):
 
     def emptyline(self):
         """Handles empty lines by doing nothing"""
+        pass
 
     def do_EOF(self, line: str):
         """Handles EOF (Ctrl+D) to exit the shell"""
